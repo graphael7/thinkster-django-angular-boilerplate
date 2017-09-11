f29d5a5efe5876443076dcdad7d137e2249cff8f
Gibral Raphael <graphael@Norths-MacBook-Pro.local>
2017-09-11 17:00:17 -0400

    added angular files

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,18 @@
-angular
-  .module('thinkster', []);
+(function(){
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.config',
+      'thinkster.routes',
+      'thinkster.authentication'
+    ]);
+
+  angular
+    .module('thinkster.routes',['ngRoute']);
+
+  angular
+    .module('thinkster.config', []);
+
+
+})();
